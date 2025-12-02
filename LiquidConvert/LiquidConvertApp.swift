bc4983d5f3f59599c243419ed5d3f47a87123672
Shawn Rain <shawnrain@foxmail.com>
2025-12-02 09:03:59 +0800

    Initial Commit

---
+++ LiquidConvert/LiquidConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidConvertApp.swift
+//  LiquidConvert
+//
+//  Created by Shawn Rain on 2025/12/2.
+//
+
+import SwiftUI
+
+@main
+struct LiquidConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
